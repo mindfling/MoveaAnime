bd1497d0487b2bdf9884fa628d9b8abd4702a984
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2026-03-13 21:06:48 +0300

    feat init folders

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// import '/css/style.css';
+import javascriptLogo from '../../public/img/javascript.svg';
+import viteLogo from '../../public/img/vite.svg';
+import { setupCounter } from './counter';
 
 document.querySelector('#app').innerHTML = `
   <div>
